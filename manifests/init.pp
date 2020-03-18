9b162c9423e687a8b3c1bd33e7fbbe34a3c3ab86
ryan <rcorales@traxiontech.net>
2020-03-18 15:45:51 +0800

    8 commit

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,10 @@
 class localusers {
-
+        user { 'admin':
+                ensure      => present,
+                shell       =>  '/bin/bash',
+                home        =>  '/home/admin',
+                gid         =>  'wheel',
+                managehome  =>  true,
+                password    =>  '$6$c6MzfrYi$KZuYcOh4XrVmd6LbeL/PK0UeLslgPgZADc2Vbh34UWBJe6Z3XCm49pN9jBmXP1T.7uPN6xn20JnelOOkqZu/7/',
+        }
 }
